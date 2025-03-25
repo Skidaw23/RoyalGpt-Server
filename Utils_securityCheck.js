5dc4dad3970f9bee4cc4ed6cb14f6a4881e1239e
Pro Jokhoe <Pro.jokhoe2@gmail.com>
2025-03-25 08:36:24 +0100

    Delete Utils_securityCheck.js

--- Utils_securityCheck.js
+++
@@ -1,8 +0,0 @@
-function isSuspiciousOrder(email, total) {
-    const suspiciousDomains = ['tempmail.com', 'disposablemail.com'];
-    const domain = email.split('@')[1];
-    const isSuspicious = suspiciousDomains.includes(domain.toLowerCase()) || parseFloat(total) > 500;
-    return isSuspicious;
-}
-
-module.exports = { isSuspiciousOrder };
